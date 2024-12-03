1e7a96561f112d34167ffe381e3b2cb488aab7d2
davijr <davioliveiraj@hotmail.com>
2024-12-02 21:22:18 -0300

    fix: Entity classes compilation error

--- src/main/java/br/com/fiap/grupo30/fastfood/customer_api/CustomerApiApplication.java
+++ src/main/java/br/com/fiap/grupo30/fastfood/customer_api/CustomerApiApplication.java
@@ -4,14 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(title = "Customer API", version = "v1"),
-        servers = {
-            @Server(url = "http://localhost:8080", description = "Local Development"),
-            @Server(
-                    url = "https://29glms05ff.execute-api.us-east-1.amazonaws.com",
-                    description = "Production Server")
-        })
 public class CustomerApiApplication {
 
     public static void main(String[] args) {
