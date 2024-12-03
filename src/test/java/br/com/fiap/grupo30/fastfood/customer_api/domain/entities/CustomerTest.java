2dcfcb64fb020534706319e327ec3476ea25582b
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-12-03 00:14:27 -0300

    test: add descriptions

--- src/test/java/br/com/fiap/grupo30/fastfood/customer_api/domain/entities/CustomerTest.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/customer_api/domain/entities/CustomerTest.java
@@ -25,64 +25,64 @@ class CustomerTest {
         Customer createdCustomer =
                 Customer.create("Jane Doe", "81704243050", "janedoe@example.com");
 
-        assertNotNull(createdCustomer);
-        assertNull(createdCustomer.getId());
-        assertEquals("Jane Doe", createdCustomer.getName());
-        assertEquals("81704243050", createdCustomer.getCpf().value());
-        assertEquals("janedoe@example.com", createdCustomer.getEmail());
+        assertNotNull(createdCustomer, "Customer can not be null");
+        assertNull(createdCustomer.getId(), "Id can not be null");
+        assertEquals("Jane Doe", createdCustomer.getName(), "Names must match");
+        assertEquals("81704243050", createdCustomer.getCpf().value(), "CPF must match");
+        assertEquals("janedoe@example.com", createdCustomer.getEmail(), "Email must match");
     }
 
     @Test
     void testToDTO() {
         CustomerDTO dto = customer.toDTO();
 
-        assertNotNull(dto);
-        assertEquals(customer.getName(), dto.getName());
-        assertEquals(customer.getCpf().value(), dto.getCpf());
-        assertEquals(customer.getEmail(), dto.getEmail());
+        assertNotNull(dto, "Customer can not be null");
+        assertEquals(customer.getName(), dto.getName(), "Names must match");
+        assertEquals(customer.getCpf().value(), dto.getCpf(), "CPF must match");
+        assertEquals(customer.getEmail(), dto.getEmail(), "Email must match");
     }
 
     @Test
     void testToPersistence() {
         CustomerEntity entity = customer.toPersistence();
 
-        assertNotNull(entity);
-        assertEquals(customer.getId(), entity.getId());
-        assertEquals(customer.getName(), entity.getName());
-        assertEquals(customer.getCpf().value(), entity.getCpf());
-        assertEquals(customer.getEmail(), entity.getEmail());
+        assertNotNull(entity, "Customer can not be null");
+        assertEquals(customer.getId(), entity.getId(), "Ids must match");
+        assertEquals(customer.getName(), entity.getName(), "Names must match");
+        assertEquals(customer.getCpf().value(), entity.getCpf(), "CPF must match");
+        assertEquals(customer.getEmail(), entity.getEmail(), "Email must match");
     }
 
     @Test
     void testEquals_SameObject() {
-        assertEquals(customer, customer);
+        assertEquals(customer, customer, "Customers must match");
     }
 
     @Test
     void testEquals_DifferentObjectSameCpf() {
         Customer anotherCustomer =
                 new Customer(2L, "John Smith", new CPF("81704243050"), "johnsmith@example.com");
-        assertEquals(customer, anotherCustomer);
+        assertEquals(customer, anotherCustomer, "Customers must match");
     }
 
     @Test
     void testEquals_DifferentCpf() {
         Customer differentCustomer =
                 new Customer(3L, "Jane Doe", new CPF("56107050027"), "janedoe@example.com");
-        assertNotEquals(customer, differentCustomer);
+        assertNotEquals(customer, differentCustomer, "Customers must match");
     }
 
     @Test
     void testHashCode_SameCpf() {
         Customer anotherCustomer =
                 new Customer(2L, "John Smith", new CPF("81704243050"), "johnsmith@example.com");
-        assertEquals(customer.hashCode(), anotherCustomer.hashCode());
+        assertEquals(customer.hashCode(), anotherCustomer.hashCode(), "Customers must match");
     }
 
     @Test
     void testHashCode_DifferentCpf() {
         Customer differentCustomer =
                 new Customer(3L, "Jane Doe", new CPF("56107050027"), "janedoe@example.com");
-        assertNotEquals(customer.hashCode(), differentCustomer.hashCode());
+        assertNotEquals(customer.hashCode(), differentCustomer.hashCode(), "Customers must match");
     }
 }
