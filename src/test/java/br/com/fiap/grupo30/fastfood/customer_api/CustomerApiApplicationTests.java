462cc6cdb66d56a3eb87c964dfaec47230c64509
davijr <davioliveiraj@hotmail.com>
2024-12-02 23:59:49 -0300

    fix: error

--- src/test/java/br/com/fiap/grupo30/fastfood/customer_api/CustomerApiApplicationTests.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/customer_api/CustomerApiApplicationTests.java
@@ -1,17 +1,9 @@
 package br.com.fiap.grupo30.fastfood.customer_api;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomerApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-        int x = 1;
-        int y = 1;
-        assertEquals(x, y, "assertion failed");
-    }
 }
