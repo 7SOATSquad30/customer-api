3374b13c00c70e1aba62587bc5b14b6ec39f2c44
davijr <davioliveiraj@hotmail.com>
2024-12-02 23:56:36 -0300

    fix: assertions

--- src/test/java/br/com/fiap/grupo30/fastfood/customer_api/CustomerApiApplicationTests.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/customer_api/CustomerApiApplicationTests.java
@@ -1,13 +1,11 @@
 package br.com.fiap.grupo30.fastfood.customer_api;
 
-import br.com.fiap.grupo30.fastfood.customer_api.CustomerApiApplication;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = CustomerApiApplication.class)
+@SpringBootTest
 class CustomerApiApplicationTests {
 
     @Test
